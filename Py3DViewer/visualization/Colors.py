23944bbdc50978017147275f2d51c588c61e46a5
masdelmon72 <101636797+masdelmon72@users.noreply.github.com>
2025-02-23 12:36:29 +0100

    Update Colors.py
    3ok

--- Py3DViewer/visualization/Colors.py
+++ Py3DViewer/visualization/Colors.py
@@ -3,14 +3,14 @@ import numpy as np
 
 class colors:
     
-    teal = np.array([1, 212, 180],dtype=float64) / 255 # Changed np.float to float
-    black = np.array([0, 0, 0],dtype=float64) / 255 # Changed np.float to float
-    orange = np.array([255, 165, 0], dtype=float64) / 255 # Changed np.float to float
-    blue = np.array([0, 0, 255], dtype=float64) / 255 # Changed np.float to float
-    red = np.array([255, 0, 0], dtype=float64) / 255 # Changed np.float to float
-    green = np.array([0, 255, 0], dtype=float64) / 255 # Changed np.float to float
-    white = np.array([255, 255, 255], dtype=np.float64) / 255
-    purple = np.array([255, 0, 255], dtype=np.float64) / 255
+    teal = np.array([1, 212, 180],dtype=np.float64) / 255 # Changed np.float to float
+    black = np.array([0, 0, 0],dtype=np.float64) / 255 # Changed np.float to float
+    orange = np.array([255, 165, 0], dtype=np.float64) / 255 # Changed np.float to float
+    blue = np.array([0, 0, 255], dtype=np.float64) / 255 # Changed np.float to float
+    red = np.array([255, 0, 0], dtype=np.float64) / 255 # Changed np.float to float
+    green = np.array([0, 255, 0], dtype=np.float64) / 255 # Changed np.float to float
+    white = np.array([255, 255, 255], dtype=np.np.float64) / 255
+    purple = np.array([255, 0, 255], dtype=np.np.float64) / 255
 
 
     
@@ -38,7 +38,7 @@ class colors:
         g = np.random.randint(256) / 255.0 
         b = np.random.randint(256) / 255.0
         
-        color = np.array([r,g,b], dtype=np.float)
+        color = np.array([r,g,b], dtype=np.float64)
         
         if return_hex:
             
