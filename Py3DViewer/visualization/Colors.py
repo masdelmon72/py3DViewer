f4c78f2885a72669e83b890528b01a69fbf5ddee
masdelmon72 <101636797+masdelmon72@users.noreply.github.com>
2025-02-23 12:23:52 +0100

    Update Colors.py
    ok

--- Py3DViewer/visualization/Colors.py
+++ Py3DViewer/visualization/Colors.py
@@ -3,14 +3,16 @@ import numpy as np
 
 class colors:
     
-    teal = np.array([1, 212, 180],dtype=np.float) / 255
-    black = np.array([0, 0, 0],dtype=np.float) / 255
-    orange = np.array([255, 165, 0], dtype=np.float) / 255
-    blue = np.array([0, 0, 255], dtype=np.float) / 255
-    red = np.array([255, 0, 0], dtype=np.float) / 255
-    green = np.array([0, 255, 0], dtype=np.float) / 255
+    teal = np.array([1, 212, 180],dtype=float) / 255 # Changed np.float to float
+    black = np.array([0, 0, 0],dtype=float) / 255 # Changed np.float to float
+    orange = np.array([255, 165, 0], dtype=float) / 255 # Changed np.float to float
+    blue = np.array([0, 0, 255], dtype=float) / 255 # Changed np.float to float
+    red = np.array([255, 0, 0], dtype=float) / 255 # Changed np.float to float
+    green = np.array([0, 255, 0], dtype=float) / 255 # Changed np.float to float
     white = np.array([255, 255, 255], dtype=np.float) / 255
     purple = np.array([255, 0, 255], dtype=np.float) / 255
+
+
     
     
     def hex2rgb(hex_color):
